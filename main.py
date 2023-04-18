3dd68b9db5bc16de9bb239c35bad37281416d6d5
Boxuan Zhang <boxuan@gmail.com>
2023-04-18 10:46:13 +0200

    command line parser

---
+++ main.py
@@ -0,0 +1,34 @@
+import argparse
+import numpy as np
+
+from train_utils import train_func
+from tinytorch import Tensor, MLP, Parameter, MomentumOptimizer, Linear, Sigmoid, square_loss, log_binary_loss
+from dataset import DataLoader
+
+
+arg_parser = argparse.ArgumentParser(description='Train and test demo of MLP')
+
+
+def arguments():
+    arg_parser.add_argument("--input_data", dest="input_data", required=True)
+    arg_parser.add_argument("--num_units", dest="num_units", required=True)
+    arg_parser.add_argument("--loss_func", dest="loss_func",choices=["square_loss", "log_binary_loss"], required=True)
+    # type=lambda x :[int(y) for y in x.split(",")])
+    arg_parser.add_argument("--activation_func", choices=["sigmoid", "tanh"], dest="activation_func", default="sigmoid")
+    arg_parser.add_argument("--batch_size", dest="batch_size", type=int,default=16)
+    arg_parser.add_argument("--num_epochs", dest="num_epochs", type=int, default=50)
+    arg_parser.add_argument("--learning_rate", dest="learning_rate", type=float, default=1e-2)
+    arg_parser.add_argument("--momentum", dest="momentum", type=float, default=0.9)
+    arg_parser.add_argument("--l2_norm", dest="l2_norm", type=float, default=1e-3)
+    arg_parser.add_argument("--test_ratio", dest="test_ratio", type=float, default=0.2)
+
+def main():
+    arguments()
+    args = arg_parser.parse_args()
+    print(f"Experiments args: {vars(args)}")
+    train_func(**vars(args))
+    
+
+if __name__ == "__main__":
+    main()
+
