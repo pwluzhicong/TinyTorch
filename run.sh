fa335b7c6f8fedcbbd6f7fb94ab08afda839d20e
Boxuan Zhang <boxuan@gmail.com>
2023-04-18 14:32:19 +0200

    new param

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@ python main.py --input_data=./data/yacht.data --num_units=32,16 \
 --loss_func=square_loss --activation_func=sigmoid --batch_size=16 \
 --num_epochs=50 --learning_rate=1e-2 --momentum=0.9 \
 --l2_norm=1e-3 --test_ratio=0.2 \
---plot_to_file plot1.png
+--plot_to_file=plot1.png
 
 python main.py --input_data=./data/BreastCancer.data --num_units=32,16,8 \
 --loss_func=log_binary_loss --activation_func=sigmoid --batch_size=16 \
 --num_epochs=50 --learning_rate=1e-2 --momentum=0.9 \
 --l2_norm=1e-3 --test_ratio=0.2 \
---plot_to_file plot2.png
+--plot_to_file=plot2.png
