2cd7db3c48d95f1975e4edbd268ffb150ea071e9
pwluzhicong <01183727@pw.edu.pl>
2023-04-18 09:43:51 +0200

    data

--- data_formatter.py
+++ data_formatter.py
@@ -1,23 +1,32 @@
 import pandas as pd
-wdbc = pd.read_csv("data/wdbc.data", header=None)
+wdbc = pd.read_csv("data/origin_data/wdbc.data", header=None)
 
 wdbc.columns = ["ID", "Diagnosis", 
            "radius-mean", "texture-mean", "perimeter-mean", "area-mean", "smoothness-mean", "compactness-mean", "concavity-mean", "concave points-mean", "symmetry-mean", "fractal dimension-mean", 
            "radius-err", "texture-err", "perimeter-err", "area-err", "smoothness-err", "compactness-err", "concavity-err", "concave points-err", "symmetry-err", "fractal dimension-err", 
            "radius-max", "texture-max", "perimeter-max", "area-max", "smoothness-max", "compactness-max", "concavity-max", "concave points-max", "symmetry-max", "fractal dimension-max"]
 
-label = wdbc["Diagnosis"] 
-features = wdbc[wdbc.columns[2:]]
+label = (wdbc["Diagnosis"] == 'M').astype(int)
+features = wdbc[wdbc.columns[2:]].copy()
 new_db = features
-new_db["label"] = label
+new_db.loc[:, "label"] = label
+
 new_db.to_csv("./data/BreastCancer.data",sep=",", index=None)
 
 
-parkinsons = pd.read_csv("data/parkinsons_updrs.data")
-d = parkinsons[["age","sex","test_time"]]
+parkinsons = pd.read_csv("data/origin_data/parkinsons_updrs.data")
+d = parkinsons[["age","sex","test_time"]].copy()
 pks = parkinsons[parkinsons.columns[6:]]
 d[parkinsons.columns[6:]] = parkinsons[parkinsons.columns[6:]]
-d["label"] = parkinsons["total_UPDRS"]
+
+
+# df['zscore'] = (df.a - df.a.mean())/df.a.std(ddof=0)
+
+d.loc[:, "label"] = parkinsons["total_UPDRS"]
+
+
+for col in d.columns:
+    d[col] = (d[col] - d[col].mean()) / d[col].std(ddof=0)
 
 d.to_csv("./data/Parkinsons.data",sep=",", index=None)
 
--- dataset.py
+++ dataset.py
@@ -1,4 +1,4 @@
-import pandas as pd
+# import pandas as pd
 import numpy as np
 
 class Dataset(object):
@@ -13,25 +13,44 @@ class Dataset(object):
 
 
 class DataLoader(object):
-    def __init__(self, dataset: Dataset, batch_size=None, shuffle=False) -> None:
+    def __init__(self, dataset: Dataset, batch_size=None, shuffle=False, random_state=None) -> None:
         self.dataset = dataset
         self.batch_size = batch_size
-        self.shuffle = shuffle
+        self.shuffle_flag = shuffle
         self.size = len(self.dataset)
         if self.batch_size:
-            self.size = np.ceil(len(self.dataset) / self.batch_size)
+            self.batch_number = np.ceil(len(self.dataset) / self.batch_size)
+        self.seed(random_state)
+        
+            
+    def seed(self, x):
+        np.random.seed(x)
+            
+    def shuffle(self):
+        self.dataset = self.dataset[np.random.choice(range(len(self.dataset)), size=len(self.dataset), replace=False)]
     
     def __iter__(self):
         if self.shuffle:
-            np.random.seed()
-            np.random.choice(range(len(self.dataset)), size=len(self.dataset), replace=False)
+            self.shuffle()
+            # np.random.seed()
+            # self.dataset = self.dataset[np.random.choice(range(len(self.dataset)), size=len(self.dataset), replace=False)]
         self._idx=-1
         return self
 
     def __next__(self):
         self._idx += 1
-        return self.dataset[]
-
+        
+        if self._idx >= self.batch_number:
+            raise StopIteration 
+        
+        sample = self.dataset[self._idx * self.batch_size: min((self._idx+1) * self.batch_size, self.size)]
+        return sample[:, :-1],  sample[:,-1]
+        
+        
+        # return self.dataset[]
+        
+    
+    
 
 class Iter(object):
     def __init__(self) -> None:
