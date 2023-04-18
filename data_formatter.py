f40c9bbd9d723c53526815327aac3944bb8eb034
Boxuan Zhang <boxuan@gmail.com>
2023-04-18 12:02:29 +0200

    new dataset

--- data_formatter.py
+++ data_formatter.py
@@ -1,36 +1,41 @@
 import pandas as pd
-wdbc = pd.read_csv("data/origin_data/wdbc.data", header=None)
+# wdbc = pd.read_csv("data/origin_data/wdbc.data", header=None)
 
-wdbc.columns = ["ID", "Diagnosis", 
-           "radius-mean", "texture-mean", "perimeter-mean", "area-mean", "smoothness-mean", "compactness-mean", "concavity-mean", "concave points-mean", "symmetry-mean", "fractal dimension-mean", 
-           "radius-err", "texture-err", "perimeter-err", "area-err", "smoothness-err", "compactness-err", "concavity-err", "concave points-err", "symmetry-err", "fractal dimension-err", 
-           "radius-max", "texture-max", "perimeter-max", "area-max", "smoothness-max", "compactness-max", "concavity-max", "concave points-max", "symmetry-max", "fractal dimension-max"]
+# wdbc.columns = ["ID", "Diagnosis", 
+#            "radius-mean", "texture-mean", "perimeter-mean", "area-mean", "smoothness-mean", "compactness-mean", "concavity-mean", "concave points-mean", "symmetry-mean", "fractal dimension-mean", 
+#            "radius-err", "texture-err", "perimeter-err", "area-err", "smoothness-err", "compactness-err", "concavity-err", "concave points-err", "symmetry-err", "fractal dimension-err", 
+#            "radius-max", "texture-max", "perimeter-max", "area-max", "smoothness-max", "compactness-max", "concavity-max", "concave points-max", "symmetry-max", "fractal dimension-max"]
 
-label = (wdbc["Diagnosis"] == 'M').astype(int)
-features = wdbc[wdbc.columns[2:]].copy()
-new_db = features
+# label = (wdbc["Diagnosis"] == 'M').astype(int)
+# features = wdbc[wdbc.columns[2:]].copy()
+# new_db = features
 
-for col in new_db.columns:
-    new_db.loc[:, col] = (new_db[col] - new_db[col].mean()) / new_db[col].std(ddof=0)
+# for col in new_db.columns:
+#     new_db.loc[:, col] = (new_db[col] - new_db[col].mean()) / new_db[col].std(ddof=0)
 
-new_db.loc[:, "label"] = label
+# new_db.loc[:, "label"] = label
 
-new_db.to_csv("./data/BreastCancer.data",sep=",", index=None)
-
-
-parkinsons = pd.read_csv("data/origin_data/parkinsons_updrs.data")
-d = parkinsons[["age","sex","test_time"]].copy()
-pks = parkinsons[parkinsons.columns[6:]]
-d[parkinsons.columns[6:]] = parkinsons[parkinsons.columns[6:]]
+# new_db.to_csv("./data/BreastCancer.data",sep=",", index=None)
 
 
-# df['zscore'] = (df.a - df.a.mean())/df.a.std(ddof=0)
+# parkinsons = pd.read_csv("data/origin_data/parkinsons_updrs.data")
+# d = parkinsons[["age","sex","test_time"]].copy()
+# pks = parkinsons[parkinsons.columns[6:]]
+# d[parkinsons.columns[6:]] = parkinsons[parkinsons.columns[6:]]
 
-d.loc[:, "label"] = parkinsons["total_UPDRS"]
+
+# # df['zscore'] = (df.a - df.a.mean())/df.a.std(ddof=0)
+
+# d.loc[:, "label"] = parkinsons["total_UPDRS"]
 
 
-for col in d.columns:
-    d.loc[:, col] = (d[col] - d[col].mean()) / d[col].std(ddof=0)
+# for col in d.columns:
+#     d.loc[:, col] = (d[col] - d[col].mean()) / d[col].std(ddof=0)
 
-d.to_csv("./data/Parkinsons.data",sep=",", index=None)
+# d.to_csv("./data/Parkinsons.data",sep=",", index=None)
 
+yacht = pd.read_csv("./data/origin_data/yacht_hydrodynamics.data", sep=" ", header=None)
+yacht.columns=["Longitudinal", "Prismatic", "Length-displacement", "Beam-draught", "Length-beam", "Froude", "label"]
+for col in yacht.columns:
+    yacht.loc[:, col] = (yacht[col] - yacht[col].mean()) / yacht[col].std(ddof=0)
+yacht.to_csv("./data/yacht.data",sep=",", index=None)
